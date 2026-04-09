5fd565095118a26ba791ab927fc4b2e44a0551a2
jhyxs <jhayalexandercdelacruz@gmail.com>
2026-04-09 21:12:50 +0800

    Add files via upload
    Checks if the statement is negative or positive.

---
+++ IfElse.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int num;
+	cout<<"Enter a number:";
+	cin>>num;
+	
+	if (num >= 0){
+		cout<<"Postive number.";
+	}
+	else {
+		cout<<"Negative number.";
+	}
+	return 0;
+}
