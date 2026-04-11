9b59c06df4ec5e2bb517d68db2f3b977b8a66c2d
jhyxs <jhayalexandercdelacruz@gmail.com>
2026-04-11 21:51:54 +0800

    Add files via upload

---
+++ Indention.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout<<"This is the difference of w/ and w/o indenetion:\n\n";
+	cout<<"Without Indention\n";
+	cout<<"\t With Indention\n";
+return 0;
+}
