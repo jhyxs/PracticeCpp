69264c3bff8364df2d436ab651a8a13ff79ebb0d
jhyxs <jhayalexandercdelacruz@gmail.com>
2026-04-11 21:51:33 +0800

    Delete Indention.cpp

--- Indention.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-	cout<<"This is the difference of w/ and w/o indenetion:\n\n";
-	cout<<"Without Indention\n";
-	cout<<"\t With Indention\n";
-return 0;
-}
