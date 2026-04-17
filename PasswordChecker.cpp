481c49daecc40d58e06bbe5ede54dd0e5bd8f756
jhyxs <jhayalexandercdelacruz@gmail.com>
2026-04-17 21:33:51 +0800

    Add files via upload

---
+++ PasswordChecker.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int pass;
+        cout <<"Enter the password: ";
+        cin >> pass;
+        
+    if (pass == 1019) {
+        cout <<"Access Granted!";
+    } else {
+        cout <<"Access Denied!";
+    }
+return 0;
+}  
+    
