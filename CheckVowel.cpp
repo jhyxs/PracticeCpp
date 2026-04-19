f6aba690ef2e3aa1aec72bfa8756f97e6a24d3eb
jhyxs <jhayalexandercdelacruz@gmail.com>
2026-04-19 18:35:15 +0800

    Add files via upload

---
+++ CheckVowel.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+    cout << "Enter a letter: ";
+    cin >> ch;
+
+    if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' ||
+        ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U') {
+        cout << "The letter is a vowel." << endl;
+    } else {
+        cout << "The letter is a consonant." << endl;
+    }
+    return 0;
+}
